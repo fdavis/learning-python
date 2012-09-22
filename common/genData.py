5ba9bebe4b3533739d62d7be28a55f204bbeb116
fdavis <reddot357@hotmail.com>
2012-09-22 11:32:10 -0700

    Continued development on random int generator

--- common/genData.py
+++ common/genData.py
@@ -1,30 +1,64 @@
 #!/usr/bin/python
 
-from optparse import OptionParser
+"""Random Integer Generator CLI Tool.
+"""
 
-parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename",
-                  help="write report to FILE", metavar="FILE")
-parser.add_option("-q", "--quiet",
-                  action="store_false", dest="verbose", default=True,
-                  help="don't print status messages to stdout")
+# need simple random generator
+def gen_random(min,max,nums):
+    from binascii import hexlify
+    from os import urandom
 
-(options, args) = parser.parse_args()
-print options, args
+    if min > max:
+        (min, max) = (max, min)
+    spread = max - min + 1
 
-def test_func(a,
-	      snarp,
-	      jarp,
-	      snack):
-	print "test func"
-
-def other_fc(
-	berker,
-	snarp):
-	print "other func"
+    for x in range(1,nums):
+        yield long(hexlify(urandom(16)),16) % spread + min
 
 
-#test_func(1,1,1,1)
-#other_fc(1,1)
-	
+# need shuffled integers generator
 
+def main():
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-M", "--max", dest="max", default=1000, type="int",
+                      help="max random number value", metavar="M")
+    parser.add_option("-m", "--min", dest="min", default=1, type="int",
+                      help="min random number value", metavar="m")
+    parser.add_option("-n", "--numbers", dest="nums", default=5, type="int",
+                      help="generate n random numbers", metavar="n")
+    parser.add_option("-u", "--unique", dest="unique", action="store_true",
+                      help="do not allow repeated numbers", metavar="u")
+    parser.add_option('-v', action="count", dest='verbosity',
+                      help="verbosity levels 0:normal, 1:details, 2:debug")
+    (options, args) = parser.parse_args()
+
+    # We usr write_details and write_debug for verbosity
+    def print_msgs(*msgs):
+        for msg in msgs:
+            print msg
+        #print
+
+    if options.verbosity > 0:
+        def write_details(*msgs):
+            print_msgs(*msgs)
+    else:
+        def write_details(*msgs): None
+
+    if options.verbosity > 1:
+        def write_debug(*msgs):
+            print_msgs(*msgs)
+    else:
+        def write_debug(*msgs): None
+
+    write_details("Called with")
+    write_details(options)
+
+    for x in range(1,options.nums):
+        print gen_random(options.min,options.max,options.nums).next()
+
+
+if __name__ == '__main__':
+    main()
